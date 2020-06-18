764c8711e835a8ffa4fb6f46a6c630a33ff649bf
Alfonso Pérez Escudero <alfonso.perez.escudero@gmail.com>
2020-06-18 09:00:50 +0200

    Add input arguments ACCESS_TOKEN and sandbox

--- Request_report_generator/zenodo.py
+++ Request_report_generator/zenodo.py
@@ -1,15 +1,27 @@
+import os
+
 import requests
 import json
 
 
-def preReserveDOI(ACCESS_TOKEN):
-    # preReserveDOI return a unpublished concetDOI and a version DOI 
+def test(thing):
+    if thing:
+        ACCESS_TOKEN = os.environ['ZENODO-API-KEY']
 
+def preReserveDOI(ACCESS_TOKEN=[],sandbox=False):
+    # preReserveDOI return a unpublished conceptDOI and a version DOI 
+    
+    if not(ACCESS_TOKEN):
+        ACCESS_TOKEN = os.environ['ZENODO-API-KEY']
+    
     # Production comment this 4 lines
-    ACCESS_TOKEN = 'USEYOURTOKEN'
+    # ACCESS_TOKEN = 'USEYOURTOKEN'
     params = {'access_token': ACCESS_TOKEN, 'prereserve_doi': 'true'}
     headers = {"Content-Type": "application/json"}
-    url = 'https://sandbox.zenodo.org/api/deposit/depositions'
+    if sandbox:
+        url = 'https://sandbox.zenodo.org/api/deposit/depositions'
+    else:
+        url = 'https://zenodo.org/api/deposit/depositions'
 
     # Production uncomment this 4 lines
     # ACCESS_TOKEN = 'USEYOURTOKEN'
@@ -23,6 +35,7 @@ def preReserveDOI(ACCESS_TOKEN):
                       headers=headers)
 
     data = r.json()
+    print(data)
     v_doi = data['metadata']['prereserve_doi']['doi']
     c_doi = v_doi.rsplit('.', 1)[0] + '.' + str(data['conceptrecid'])
     links = data["links"]
@@ -30,7 +43,8 @@ def preReserveDOI(ACCESS_TOKEN):
     return c_doi, v_doi, links
 
 
-def publish(document_path, filename, links, metadata):
+def publish(document_path, filename, links, metadata,ACCESS_TOKEN=[],
+            sandbox=False):
     # metadata example:
     # {'metadata': {'title': 'My first upload',
     #                      'upload_type': 'poster',
@@ -38,8 +52,10 @@ def publish(document_path, filename, links, metadata):
     #                      'creators': [{'name': 'Doe, John',
     #                                   'affiliation': 'Zenodo'}]}}
     # Publish the document
+    if not(ACCESS_TOKEN):
+        ACCESS_TOKEN = os.environ['ZENODO-API-KEY']
     bucket_url = links['bucket']
-    ACCESS_TOKEN = 'USEYOURTOKEN'
+    # ACCESS_TOKEN = 'USEYOURTOKEN'
     params = {'access_token': ACCESS_TOKEN}
     headers = {"Content-Type": "application/json"}
     with open(document_path, "rb") as fp:
@@ -62,11 +78,14 @@ def publish(document_path, filename, links, metadata):
 
 
 if __name__ == '__main__':
-    c_doi, v_doi, links = preReserveDOI()
+    ACCESS_TOKEN = 'ZkSjaf1ZxmHstib0YxW8N6kQN9uDWaoaYlWJcnpZ00HqZYsQ05iUHnQ1FuiI' # Alfonso's sandbox credentials
+    c_doi, v_doi, links = preReserveDOI(ACCESS_TOKEN=ACCESS_TOKEN,
+                                        sandbox=True)
     metatdata = {'metadata': {'title': 'My first upload',
                               'upload_type': 'poster',
                               'description': 'This is my first upload',
                               'creators': [{'name': 'Doe, John',
                                             'affiliation': 'Zenodo'}]}}
 
-    publish('xxx_report.pdf', 'xxx_report.pdf', links, metatdata)
+    publish('xxx_report.pdf', 'xxx_report.pdf', links, metatdata,
+            ACCESS_TOKEN=ACCESS_TOKEN, sandbox=True)
